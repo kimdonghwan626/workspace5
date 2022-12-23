a3bb60a82ff95343c2d6929b81ddcc7cbf5b5744
NB-21122703\NB-000000 <donghwan.kim@bankwareglobal.com>
2022-12-23 23:59:51 +0900

    sdsdv

--- testProject/src/testProject/dbio/dbio.java
+++ testProject/src/testProject/dbio/dbio.java
@@ -2,7 +2,7 @@
  * DBIO 에디터에서 생성된 파일입니다. 인터페이스 파일을 직접 수정하지 마십시오.
  * @Generated Fri Dec 23 22:02:56 KST 2022
  */
-package testProject.dbio2;
+package testProject.dbio;
 
 import bxm.common.annotaion.BxmCategory;
 import bxm.container.annotation.BxmDataAccess;
@@ -10,7 +10,7 @@ import bxm.container.annotation.BxmDataAccess;
 /**
  */
 @SuppressWarnings({ "all" })
-@BxmDataAccess(mapper = "testProject/dbio2/dbio.dbio", datasource = "MainDS")
+@BxmDataAccess(mapper = "testProject/dbio/dbio.dbio", datasource = "MainDS")
 @BxmCategory(logicalName = "dbio", author = "sysadmin")
 public interface dbio
 {
